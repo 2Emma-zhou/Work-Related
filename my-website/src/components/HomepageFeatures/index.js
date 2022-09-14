350cf20ca2fdf791bbe8f450cee787d186f28369
2Emma-zhou <1051725904@qq.com>
2022-09-14 11:17:53 +0800

    change settings

--- my-website/src/components/HomepageFeatures/index.js
+++ my-website/src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Focus on Content',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Technical documentation places most of its focus on the content.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'User Oriented',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Think like a user, write like an expert.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Powered by Enthusiasm',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Always excited and enthusiastic to make technical stuff easy for everyone.
       </>
     ),
   },
