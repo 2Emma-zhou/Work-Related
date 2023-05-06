564d4931e5ef357f5fabdb090ed03e9377b8b369
2Emma-zhou <1051725904@qq.com>
2023-05-06 16:30:54 +0800

    test build without user management

--- my-website/sidebars.js
+++ my-website/sidebars.js
@@ -322,16 +322,6 @@ const sidebars = {
   },
   {
     type: 'category',
-      label: 'User Management',
-      link: {
-          type: 'generated-index',
-      },
-      items: [
-    'Open API/User Management/Users',
-    ]
-  },
-  {
-    type: 'category',
       label: 'Identity Authentication',
       link: {
           type: 'generated-index',
